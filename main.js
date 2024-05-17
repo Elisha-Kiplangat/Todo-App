99cb0033aafdda2e7817324b7aef003bcb57a90d
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-05-17 11:41:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const pege = document.DOMContentLoaded(() => {
+    const dlt = document.querySelector('.clear');
+
+    dlt.addEventListener('click', (e) => {
+        e.target.parentElement.parentElement.remove();
+    });
+
+    const add = document.querySelector('input').value;
+
+    
+
+
+});
